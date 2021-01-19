091dfb51e8e10c9c794acd7a993a728985f4af50
nao20010128nao <nao20010128@gmail.com>
2021-01-19 11:26:09 +0000

    detect end of live

--- api/live/[filename].js
+++ api/live/[filename].js
@@ -49,7 +49,7 @@ module.exports = async (req, res) => {
     if (contentType == "application/vnd.apple.mpegurl") {
       data = Buffer.from(data)
         .toString("utf8")
-        .replace(/([^:\r\n]+\.(ts|m3u8))$/gm, function (match, g1) {
+        .replace(/([^:\r\n]+\.(?:ts|m3u8))$/gm, function (match, g1) {
           const query = qs.stringify({
             __guest_id,
             __location,
@@ -63,8 +63,12 @@ module.exports = async (req, res) => {
             is_lhls,
           });
           return `https://bookish-octo-barnacle.vercel.app/api/live/${g1}?${query}`;
-        })
-        .replace(/#EXT-X-PREFETCH:/g, "");
+        });
+      const noPrefetch = data.replace(/#EXT-X-PREFETCH:/g, "");
+      if (data == noPrefetch) {
+        res.status(404);
+      }
+      data = noPrefetch;
     }
     res.send(data);
   } catch (e) {
