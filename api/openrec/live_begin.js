ba190312d8360c68fedb0ef7cd26d2fbc4154f28
nao20010128nao <nao20010128@gmail.com>
2021-02-11 10:52:55 +0000

    remove some section in M3U8

--- api/openrec/live_begin.js
+++ api/openrec/live_begin.js
@@ -24,7 +24,7 @@ module.exports = async (req, res) => {
     const lastSegmentNumber = parseInt(lastSegment.match(lastNumber));
     console.log(lastSegmentNumber);
 
-    let result = preambleText;
+    let result = "#EXTM3U\n#EXT-X-VERSION:3\n"; //preambleText;
 
     for (let num = 0; num <= lastSegmentNumber; num++) {
       const segName = lastSegment.replace(lastNumber, `${num}.ts`);
