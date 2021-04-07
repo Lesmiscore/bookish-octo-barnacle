0529f1c78e85de2ef878f9283c1047badc211a45
nao20010128nao <nao20010128@gmail.com>
2021-04-07 21:45:56 +0900

    add mplus font
    https://github.com/rinsuki/nicothumb2img

--- api/google_search.js
+++ api/google_search.js
@@ -1,6 +1,22 @@
 const chromium = require("chrome-aws-lambda");
+const fs = require("fs");
+const path = require("path");
+
+
+function chromiumFontSetup() {
+  if (process.env.HOME == null) process.env.HOME = "/tmp"
+  const dest = process.env.HOME + "/.fonts"
+  if (!fs.existsSync(dest)) fs.mkdirSync(dest)
+  const src = __dirname + "/../fonts/mplus"
+  for (const font of fs.readdirSync(src)) {
+    if (!font.endsWith(".ttf")) continue
+    if (fs.existsSync(path.join(dest, font))) continue
+    fs.copyFileSync(path.join(src, font), path.join(dest, font))
+  }
+}
 
 module.exports = async (req, res) => {
+  chromiumFontSetup() ;
   const { q } = req.query;
 
   const { puppeteer } = chromium;
@@ -15,7 +31,7 @@ module.exports = async (req, res) => {
     res.setHeader('Content-Type', 'image/png');
     await page.goto(`https://google.com/search?q=${encodeURIComponent(q)}`);
     console.log("loaded");
-    await new Promise(r => setTimeout(r, 5000));
+    await new Promise(r => setTimeout(r, 4000));
     const screenshot = await page.screenshot({ type: "png" });
     res.send(screenshot);
   } finally {
