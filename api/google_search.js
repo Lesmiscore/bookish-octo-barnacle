3fc036e8c1978d7a02c4ee214c9622aaab04bdfa
nao20010128nao <nao20010128@gmail.com>
2021-04-09 14:11:37 +0900

    wait conditionally

--- api/google_search.js
+++ api/google_search.js
@@ -15,6 +15,17 @@ function chromiumFontSetup() {
   }
 }
 
+async function measureTime(func) {
+  let t0, t1, ret;
+  try {
+    t0 = performance.now();
+    ret = await func();
+  } finally {
+    t1 = performance.now();
+  }
+  return [ret, t1 - t0];
+}
+
 const negative = [
   /nao20010128nao/
 ];
@@ -42,8 +53,12 @@ module.exports = async (req, res) => {
     res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
     res.setHeader("Content-Type", "image/png");
     await page.setViewport({ width: 1024, height: 768, deviceScaleFactor: 4 });
-    await page.goto(`https://google.com/search?q=${encodeURIComponent(q)}`);
-    await new Promise(r => setTimeout(r, 500));
+    const [_, time] = measureTime(async () => {
+      await page.goto(`https://google.com/search?q=${encodeURIComponent(q)}`);
+    });
+    if (time < 2000) {
+      await new Promise(r => setTimeout(r, 500));
+    }
     const screenshot = await page.screenshot({ type: "png" });
     res.send(screenshot);
   } finally {
