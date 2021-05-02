6c481c9d39c523593ac94d72f56ce00ce3762d39
nao20010128nao <nao20010128@gmail.com>
2021-05-02 13:04:58 +0900

    remove endpoints

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ app.get('/', require("./api/index.js"));
 app.get('/api/', require("./api/index.js"));
 app.get('/api/google_search', require("./api/google_search.js"));
 
-app.get('/api/mildom/dispatcher_config', require("./api/mildom/dispatcher_config.js"));
-app.get('/api/mildom/live/:filename', require("./api/mildom/live/[filename].js"));
-app.get('/api/mildom/vod2/proxy', require("./api/mildom/vod2/proxy.js"));
-
 app.get('/api/openrec/live_begin', require("./api/openrec/live_begin.js"));
 
 app.get('/api/y2mate/youtube', require("./api/y2mate/youtube.js"));
