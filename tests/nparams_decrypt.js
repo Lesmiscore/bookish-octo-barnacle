5771f511d39aaf6d2d8ad92b8e71f2a4b89db807
nao20010128nao <nao20010128@gmail.com>
2021-10-16 10:08:27 +0900

    adding test case for n-param decryption

---
+++ tests/nparams_decrypt.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const decrypt = require("../api/youtube/nparams/decrypt.js");
+
+describe('decrypts n params', function () {
+  for (file of fs.readdirSync('./tests/youtube_playerjs/')) {
+    it(file, function () {
+      const jscode = fs.readFileSync('./tests/youtube_playerjs/' + file);
+      decrypt.decryptNParam(jscode, '12345678');
+    })
+  }
+});
