860d51ea336f5377f725a53908cdc8cd7707d3a2
nao20010128nao <nao20010128@gmail.com>
2021-10-16 23:29:45 +0900

    be more permissive

--- api/youtube/nparams/decrypt.js
+++ api/youtube/nparams/decrypt.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const nParamFuncName = [/\.get\("n"\)\)&&\(([a-zA-Z0-9$]+)=([a-zA-Z0-9$]{3,})\(\1\)/];
-const nParamFuncBody = [/[FUNCNAME]=(function\([a-zA-Z0-9$]+\)\{.+?return [a-zA-Z0-9$]+\.join\(['"]{2}\)\};?)$/ms];
+const nParamFuncBody = [/[FUNCNAME]=(function\([a-zA-Z0-9$]+\)\{.+?return [a-zA-Z0-9$]+\.join\(['"]{2}\)\};?)/ms];
 
 class NDecryptError extends Error {
   constructor(step, msg) {
