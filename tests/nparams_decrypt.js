094c23521571f406be981b8f97441901e4f6717c
nao20010128nao <nao20010128@gmail.com>
2021-10-27 16:30:43 +0900

    rename js files

--- tests/nparams_decrypt.js
+++ tests/nparams_decrypt.js
@@ -6,7 +6,7 @@ describe('decrypts n params', function () {
   for (file of fs.readdirSync('./tests/youtube_playerjs/')) {
     it(file, function () {
       const jscode = fs.readFileSync('./tests/youtube_playerjs/' + file);
-      decrypt.decryptNParam(jscode, '12345678');
+      decrypt.decryptNParam(jscode, 'iozK6raRyrJcxIfjM');
     })
   }
 });
